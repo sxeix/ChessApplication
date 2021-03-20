7c4c4fa023768181bd79815482d4e7d798526f4e
sxeix <sxeixarts@gmail.com>
2021-03-20 22:13:28 +0000

    Initial Commit

---
+++ src/main/java/Chess.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Chess extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label l = new Label("ChessApplication");
+        Scene scene = new Scene(new StackPane(l), 600, 600);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
