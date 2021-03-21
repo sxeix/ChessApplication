e0862885f493c74e618dbac4ec7b6f73223fca8f
sxeix <sxeixarts@gmail.com>
2021-03-21 21:08:40 +0000

    Add movement for multiple pieces

--- src/main/java/Chessboard.java
+++ src/main/java/Chessboard.java
@@ -1,11 +1,14 @@
 import enums.ColourEnum;
 import enums.PieceEnum;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
 
 @RequiredArgsConstructor
 public class Chessboard {
@@ -19,7 +22,7 @@ public class Chessboard {
     @Getter
     private GridPane board;
 
-    double orgSceneX, orgSceneY;
+    private ArrayList<ChessPiece> pieces = new ArrayList<>();
 
     public void initBoard() {
         GridPane grid = new GridPane();
@@ -45,8 +48,20 @@ public class Chessboard {
     public void setPieces(GridPane grid) {
         ChessPiece king = new ChessPiece(PieceEnum.KING, ColourEnum.BLACK,3,4, pxSquareEdge);
         ChessPiece queen = new ChessPiece(PieceEnum.QUEEN, ColourEnum.WHITE,6,4, pxSquareEdge);
+        pieces.add(king);
+        pieces.add(queen);
         // Adds to board
-        grid.getChildren().add(king);
-        grid.getChildren().add(queen);
+        for(ChessPiece piece: pieces) {
+            grid.getChildren().add(piece);
+            piece.setOnMousePressed((MouseEvent event) -> {
+                board.setOnMouseDragged((MouseEvent e) -> {
+                    GridPane.setRowIndex(piece, (int) e.getY()/75);
+                    GridPane.setColumnIndex(piece, (int) e.getX()/75);
+                });
+                piece.setOnMouseReleased((MouseEvent e) -> {
+                    board.setOnMouseDragged(null);
+                });
+            });
+        }
     }
 }
