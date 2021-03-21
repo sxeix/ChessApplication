8e0fae8fd63d31d50c0eb6a7d16c4e19e0edb851
sxeix <sxeixarts@gmail.com>
2021-03-21 16:26:46 +0000

    Add test class & JUnit

---
+++ src/test/java/ChessboardTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ChessboardTest {
+
+    @Test
+    public void init_board_test() {
+        Chessboard testBoard = new Chessboard(600);
+        testBoard.initBoard();
+        assertThat(testBoard.getBoard(), notNullValue());
+    }
+
+}
